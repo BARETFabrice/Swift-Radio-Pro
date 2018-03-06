83c1dc154a7420d6f64797419d22798ba305381a
BARETFabrice <baretfabrice974@gmail.com>
2018-03-05 20:25:21 -0800

    commit

--- SwiftRadio/ConnectionViewController.swift
+++ SwiftRadio/ConnectionViewController.swift
@@ -12,7 +12,6 @@ import Foundation
 class ConnectionViewController: UIViewController {
     @IBOutlet var login : UITextField!
     @IBOutlet var password : UITextField!
-    @IBOutlet var btnConnextion : UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +19,57 @@ class ConnectionViewController: UIViewController {
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning();
+    }
+    
+    @IBAction func actionSurBtnConnection(sender : UIButton)
+    {
+        var loginEnter : Bool
+        var passwordEnter : Bool
+        
+        if let resultatLogin = login.text, resultatLogin != "" {
+            loginEnter = true;
+        }
+        else {
+            loginEnter = false;
+        }
+        
+        if let resultatPassword = password.text, resultatPassword != "" {
+            passwordEnter = true;
+        }
+        else {
+            passwordEnter = false;
+        }
+        
+        if (!loginEnter || !passwordEnter) {
+            alert(title: "Erreur", message: "login ou password vide")
+        }
+        else {
+            if isCantConnexion() {
+                afficherPanneauListeStation()
+            }
+        }
+    }
+    
+    func isCantConnexion()  -> Bool {
+        var condition : Bool
+        
+        condition = false
+        
+        if(login.text == "fabrice" && password.text == "fabrice") {
+            condition = true
+        }
+        
+        if(!condition) {
+            alert(title: "Erreur", message: "Login ou password incorect")
+        }
+        
+        return condition;
+    }
+    
+    func afficherPanneauListeStation()
+    {
+        
+        alert(title: "OK", message: "Connextion")
     }
     
     func alert(title: String, message: String) {
